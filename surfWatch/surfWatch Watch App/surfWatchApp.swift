e5f625a7d0e5ad62f532650f5353400202ca31b9
jimeNestor <nestor.jimenez.003@gmail.com>
2024-05-13 19:51:19 -0500

    Added Project folders

---
+++ surfWatch/surfWatch Watch App/surfWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  surfWatchApp.swift
+//  surfWatch Watch App
+//
+//  Created by Nestor Jimenez on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct surfWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
